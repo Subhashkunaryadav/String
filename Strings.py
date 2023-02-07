faa0ad33266453ef818f4f703f5661c6b1ce9cfe
subhash kumar yadav <110653851+Subhashkunaryadav@users.noreply.github.com>
2023-02-07 08:28:55 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,26 @@
+# Concatenation
+string1 = "Hello"
+string2 = "World"
+string3 = string1 + string2
+print(string3) # "HelloWorld"
+
+# Repetition
+string4 = string1 * 3
+print(string4) # "HelloHelloHello"
+
+# Indexing
+string5 = "Hello World"
+print(string5[0]) # "H"
+
+# Slicing
+print(string5[0:5]) # "Hello"
+
+# Length
+print(len(string5)) # 11
+
+# Formatting
+name = "John"
+print("Hello {}!".format(name)) # "Hello John!"
+
+# Methods
+print(string5.upper()) # "HELLO WORLD"
